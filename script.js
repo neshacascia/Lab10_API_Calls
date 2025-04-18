5835a56a880699f38e18f1ee1900384bd06ef37d
Nesha Mervin <nesha.mervin@gmail.com>
2025-04-17 21:47:24 -0400

    implemented get request using fetch() and displayed data in the webpage

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+const fetchBtn = document.querySelector('#fetch-btn');
 
+fetchBtn.addEventListener('click', fetchData);
+
+function fetchData() {
+  fetch('https://jsonplaceholder.typicode.com/posts/1')
+    .then(res => {
+      if (!res.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return res.json();
+    })
+    .then(data => {
+      console.log(data);
+      const { title, body } = data;
+      displayData(title, body);
+    })
+    .catch(err => console.error('Error fetching data:', err));
+}
+
+function displayData(title, body) {
+  const titleEl = document.querySelector('#title');
+  const bodyEl = document.querySelector('#body');
+
+  titleEl.innerText = title;
+  bodyEl.innerText = body;
+}
